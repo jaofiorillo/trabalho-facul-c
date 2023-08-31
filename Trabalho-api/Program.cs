b0d1ca905b738e068462564e6a40a1c9dc20d488
Fiorillo <joao.banci@xbrain.com.br>
2023-08-31 20:11:52 -0300

    commit inicial

---
+++ Trabalho-api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Trabalho_api.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var connexao = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddDbContext<ApplicationDbContext>(
+    x => x.UseMySql(connexao, ServerVersion.Parse("8.0.1")));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Trabalho-api/teste.cs
@@ -0,0 +1,7 @@
+namespace Trabalho_api;
+
+public class teste
+{
+    public int id { get; set; }
+    public string nome { get; set; }
+}
